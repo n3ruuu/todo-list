ff5a89a410add93b3c345b1187dda8a6a52658f2
mustbeneru <neilcarlo.zapanta.d@gmail.com>
2024-07-28 22:15:17 +0800

    added functions in dom.js

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -3,55 +3,71 @@ import Project from './project.js'
 export default class DOM {
     constructor() {
         this.projects = []
-
         // cache dom
         this.sidebar = document.querySelector('.sidebar')
         this.addProjectBtn = document.querySelector('.add-project.btn')
         this.projectList = document.querySelector('ul')
-
         // bind events
         this.addProjectBtn.addEventListener('click', this.showInputField.bind(this))
     }
 
-    showInputField() {
-        if (this.projectInput) return
-        this.projectInput = document.createElement('input')
-        this.projectInput.className = 'project-input'
-        this.projectInput.type = 'text'
-
-        this.projectInput.addEventListener('keydown', (e) => {
-            if (e.key === `Enter`) {
-                this.projectName = this.projectInput.value
-                if (this.projectName === '') {
-                    alert('Enter your project name')
-                    return 
-                } 
-
-                if (this.checkName(this.projectName)) {
-                    alert(`${this.projectName} already exists`)
-                    this.projectInput.value = ''
-                    return
-                }
-
-                const newProject = new Project(this.projectName)
-                this.projects.push(newProject)
-                alert(`${newProject.title} is added succcessfully!`)
-
-                const li = document.createElement('li')
-                li.textContent = this.projectName
-
-                this.projectList.appendChild(li)
-                this.projectInput.remove()
-                this.projectInput = null
-
-            }
-        })
-        this.projectList.appendChild(this.projectInput)
+    createInputField() {
+        const input = document.createElement('input')
+        input.className = 'project-input'
+        input.type = 'text'
+        input.focus()
+        input.addEventListener('keydown', this.handInputKeyDown.bind(this))
+        input.addEventListener('blur', this.hideInputField.bind(this))
+        return input
     }
 
-    checkName(projectName) {
+    handInputKeyDown(e) {
+        if (e.key === `Enter`) {
+            const projectName = e.target.value.trim();
+            if (this.isValid(projectName)) {
+                this.appendProject(projectName)
+            }
+            e.target.value = ''
+        }
+    }
+
+    isValid(name) {
+        if (name === '') return false
+        if (this.checkIfExists(name)) {
+            alert('Duplicate')
+            return false
+        }
+        return true
+    }
+
+    appendProject(projectName) { 
+        const newProject = new Project(projectName)
+        this.projects.push(newProject)
+
+        alert(`${newProject.title} is added succcessfully!`)
+
+        const li = document.createElement('li')
+        li.textContent = projectName
+        this.projectList.appendChild(li)
+        this.hideInputField()
+    }
+
+    showInputField() {
+        if (!this.projectInput) {
+            this.projectInput = this.createInputField()
+            this.projectList.appendChild(this.projectInput)
+        }
+    }
+
+    hideInputField() {
+        if (this.projectInput) {
+            this.projectInput.style.display = 'none';
+            this.projectInput = null
+        }
+    }
+
+    checkIfExists(projectName) {
         return this.projects.some(project => project.title === projectName)
     }
-
 }
 
